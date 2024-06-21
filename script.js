04bc6fadb96b409951639afd306281df1cec7c30
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-21 19:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	let a=str.split("").reverse().join("");
+	if(a==str){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
